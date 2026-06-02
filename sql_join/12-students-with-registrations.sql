dbab9c3706bc3396858af33fc6650cfdcb6abe8c
panaki-gillot <gillotpanaki@gmail.com>
2026-06-02 18:09:09 +0200

    refactor SQL query to join registrations table for student names

--- sql_join/12-students-with-registrations.sql
+++ sql_join/12-students-with-registrations.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
-    s.name AS student_name
-FROM students s
-INNER JOIN enrollments e
-    ON s.id = e.student_id
+    students.name AS student_name
+FROM students
+INNER JOIN registrations
+    ON students.id = registrations.student_id
 ORDER BY student_name ASC;
